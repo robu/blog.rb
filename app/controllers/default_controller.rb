907ff995d0630ada6416c1f66ebd4a6816a5b09d
Robert Burén <robert@buren.se>
2008-07-13 18:55:17 +0200

    pagination of blog posts in place

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -5,6 +5,7 @@ class DefaultController < ApplicationController
       redirect_to blogs_path
       return
     end
+    @posts = @blog.published_posts.paginate :page => params[:page], :per_page => 10
     respond_to do |format|
       format.html { render :template => 'blogs/show', :layout => 'blogs' }
       format.xml  { render :xml => @blog }
