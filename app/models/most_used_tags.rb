997bb92c27866234d4b08d793474c5dc30b38dac
Robert Burén <robert@buren.se>
2008-09-10 18:50:55 +0200

    tags in sidebar: added sorting on taggings count and a limit value

--- app/models/most_used_tags.rb
+++ app/models/most_used_tags.rb
@@ -1,6 +1,8 @@
 class MostUsedTags < SidebarComponent
-  def tags
-    self.blog.tags
+  def tags(limit=0)
+    tags = self.blog.tags
+    tags.sort {|t1,t2| diff = t2.taggings.count <=> t1.taggings.count; diff != 0 ? diff : t1.name <=> t2.name}
+    limit <= 0 || limit > tags.size ? tags : tags[0..(limit-1)]
   end
 
   def content
