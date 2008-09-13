172ce4dc5455c4ef8e81e766bbf103fa2c6c13a1
Robert Burén <robert@buren.se>
2008-09-13 10:18:18 +0200

    fixed the tag sidebar sort order

--- app/models/most_used_tags.rb
+++ app/models/most_used_tags.rb
@@ -1,8 +1,8 @@
 class MostUsedTags < SidebarComponent
   def tags(limit=0)
-    tags = self.blog.tags
-    tags.sort {|t1,t2| t2.taggings.count <=> t1.taggings.count}
-    limit <= 0 || limit > tags.size ? tags : tags[0..(limit-1)]
+    ts = self.blog.tags
+    ts.sort! {|t1,t2| t2.taggings.count <=> t1.taggings.count}
+    limit <= 0 || limit > ts.size ? ts : ts[0..(limit-1)]
   end
 
   def content
